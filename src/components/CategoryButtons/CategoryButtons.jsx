bfc0cbcff982ad44a32a3d116415068c92b4291d
Kyle Jensen <kjensen19@gmail.com>
2022-11-07 11:16:16 -0600

    added click handler with consolelog

--- src/components/CategoryButtons/CategoryButtons.jsx
+++ src/components/CategoryButtons/CategoryButtons.jsx
@@ -7,29 +7,40 @@ import './CategoryButtons.css'
 
 export default function CategoryButtons() {
 
+    const handleClick = (e) => {
+        console.log('id = ', e.target.id)
+    }
+
     return(
         <Box sx={{width: '100%'}}>
             <Grid container spacing={1} mt={3} mb={3}>
                 <Grid item xs={6}>
-                    <Button sx={{borderRadius:5, fontSize:20}} className="categoryButton" color='palePink' variant='contained'>Shoes</Button>
+                    <Button id='top' sx={{borderRadius:5, fontSize:20}} className="categoryButton" color='palePink' onClick={handleClick} variant='contained'>Tops</Button>
                 </Grid>
                 <Grid item xs={6}>
-                    <Button sx={{borderRadius:5, fontSize:20}} className="categoryButton" color='palePink' variant='contained'>Tops</Button>
+                    <Button id='bottom' sx={{borderRadius:5, fontSize:20}} className="categoryButton" color='palePink' onClick={handleClick} variant='contained'>Bottoms</Button>
                 </Grid>
                 <Grid item xs={6}>
-                    <Button sx={{borderRadius:5, fontSize:20}}  className="categoryButton" color='palePink' variant='contained'>Bottoms</Button>
+                    <Button id='footwear'sx={{borderRadius:5, fontSize:20}}  className="categoryButton" color='palePink' onClick={handleClick} variant='contained'>Footwear</Button>
                 </Grid>
                 <Grid item xs={6}>
-                    <Button sx={{borderRadius:5, fontSize:20}} className="categoryButton" color='palePink' variant='contained'>Dresses</Button>
+                    <Button id='outwear' sx={{borderRadius:5, fontSize:20}} className="categoryButton" color='palePink' onClick={handleClick} variant='contained'>Outerwear</Button>
                 </Grid>
                 <Grid item xs={6}>
-                    <Button sx={{borderRadius:5, fontSize:20}} className="categoryButton" color='palePink' variant='contained'>Hats</Button>
+                    <Button id='hat' sx={{borderRadius:5, fontSize:20}} className="categoryButton" color='palePink' onClick={handleClick} variant='contained'>Hats</Button>
                 </Grid>
                 <Grid item xs={6}>
-                    <Button sx={{borderRadius:5, fontSize:20}} className="categoryButton" color='palePink' variant='contained'>Accessories</Button>
+                    <Button id='accessories' sx={{borderRadius:5, fontSize:20}} className="categoryButton" color='palePink' onClick={handleClick} variant='contained'>Accessories</Button>
                 </Grid>
             </Grid>
         </Box>
     )
 }
 
+// ("top"),
+// ("bottom"),
+// ("footwear"),
+// ("dress"),
+// ("hat"),
+// ("outerwear"),
+// ("accessories");
