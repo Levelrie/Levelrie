c5d550fd03685e6922859bca8d2f9076bfdf1aa9
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-11-09 12:03:07 -0600

    favorite outfits rendering on dom

--- src/components/FavoriteOutfitList/FavoriteOutfitItem.jsx
+++ src/components/FavoriteOutfitList/FavoriteOutfitItem.jsx
@@ -5,13 +5,16 @@ import Stack from '@mui/material/Stack';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
 
-function FavoriteOutfitItem() {
+function FavoriteOutfitItem({outfit}) {
 
+    console.log('this is outfit in item:', {outfit})
     return (
-        <>
-            <p>Favorite Outfit List Item</p>
-        </>
+        <Paper key={outfit.id}>
+            <Typography>{outfit.name}</Typography>
+            <Typography>{outfit.description}</Typography>
+        </Paper>
     );
 };
 
