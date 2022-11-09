839bac81e97ab5252481e2dc097081c37f7fe3b8
Theo Janke-Furman <tjankefurman@gmail.com>
2022-11-09 17:13:23 -0600

    Basic swiping functionality added

--- src/components/OutfitComponents/OutfitHomeItem.jsx
+++ src/components/OutfitComponents/OutfitHomeItem.jsx
@@ -2,7 +2,7 @@
 
 import FavoriteButton from "../FavoriteButton/FavoriteButton";
 
-export default function OutfitHomeItem({outfitsArray, homeCounter}) {
+export default function OutfitHomeItem({outfit, homeCounter}) {
 
     // Determine what page we're on somehow
         // if we're on home view or favorites view -> each item needs a favorite button
@@ -13,14 +13,14 @@ export default function OutfitHomeItem({outfitsArray, homeCounter}) {
     // Assuming each outfit comes with an array_agg of all its associated items
     return (
         <>
-            <p>{outfitsArray[homeCounter]?.name}</p>
-            <p>{outfitsArray[homeCounter]?.description}</p>
-            {outfitsArray[homeCounter]?.items.map((item) => {
+            <p>{outfit.name}</p>
+            <p>{outfit.description}</p>
+            {outfit.items.map((item) => {
             return (
                 // need to set each item's position somehow -> use className?
                 //      If using class name, need to ensure items are received with category names
                 // <img key={item.f1.id} className={item.f2} src={item.f1.img} />
-                <p key={item.f1.id}>{item.f1.name} <FavoriteButton itemId={item.f1.id} outfitId={outfitsArray[homeCounter].id}/></p>
+                <p key={item.f1.id}>{item.f1.name} <FavoriteButton itemId={item.f1.id} outfitId={outfit.id}/></p>
                 );
             })}
         </>
