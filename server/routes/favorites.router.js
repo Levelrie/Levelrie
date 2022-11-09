20d9d7823ab7de58e7096ca019d5cf6201c44e12
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-11-09 11:37:58 -0600

    clean up route file

--- server/routes/favorites.router.js
+++ server/routes/favorites.router.js
@@ -1,5 +1,3 @@
-// try the below and navigate to: http://localhost:5000/api/favorites/outfits
-
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
@@ -7,12 +5,6 @@ const router = express.Router();
 const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
 console.log("fav router");
-
-// router.get('/favorites/outfits', (req, res) => {
-//     console.log('here');
-//     res.sendStatus(200);
-
-// })
 
 router.get('/', (req, res) => {
     console.log('in GET /api/favorites/outfits');
