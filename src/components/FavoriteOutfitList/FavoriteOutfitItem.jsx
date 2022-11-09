cb8e970745931f85408db032b34bcc46cef81c46
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-11-09 09:32:45 -0600

    add favorites route, saga, reducer

---
+++ src/components/FavoriteOutfitList/FavoriteOutfitItem.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+
+// MUI
+import Stack from '@mui/material/Stack';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+
+function FavoriteOutfitItem() {
+
+    return (
+        <>
+            <p>Favorite Outfit List Item</p>
+        </>
+    );
+};
+
+export default FavoriteOutfitItem;
--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import errors from './errors.reducer';
 import user from './user.reducer';
 import outfits from './outfits.reducer';
+import favorites from './favorites.reducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -12,7 +13,8 @@ import outfits from './outfits.reducer';
 const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   user, // will have an id and username if someone is logged in
-  outfits
+  outfits,
+  favorites // contains favoriteOutfitsReducer and favoriteItemsReducer
 });
 
 export default rootReducer;
