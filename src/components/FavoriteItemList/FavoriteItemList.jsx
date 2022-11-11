c94b52c4933c3b0a815c9160c56671f69196cde7
Theo Janke-Furman <tjankefurman@gmail.com>
2022-11-10 19:11:33 -0600

    Completed Favorites page switch functionality

--- src/components/FavoriteItemList/FavoriteItemList.jsx
+++ src/components/FavoriteItemList/FavoriteItemList.jsx
@@ -2,12 +2,15 @@ import { useEffect, useState } from 'react';
 import { useDispatch, useSelector} from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import FavoriteItemItem from "./FavoriteItemItem";
+import ToggleButton from '../ToggleButton/ToggleButton.jsx';
+import BottomBar from "../BottomBar/BottomBar";
 
 // MUI
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
 
 function FavoriteItemList({category}) {
     const dispatch = useDispatch();
@@ -28,17 +31,39 @@ function FavoriteItemList({category}) {
         }
     }, []);
 
+    const toggleButtonClicked = (e) => {
+        console.log('PASSED???', e.target.value);
+
+        switch(e.target.value) {
+            case 'outfit':
+                history.push('/favorites/outfits')
+                setHighlightedButton('outfit');
+                break;
+            case 'category':
+                // No need to history.push, you're already here!
+                setHighlightedButton('category');
+                break;
+        }
+
+    }
+
     console.log('favoriteItems is:', favoriteItems);
 
     return (
-        <Container>
-        <Typography variant="h6">Faves</Typography>
-        <Stack spacing={2}>
-            {favoriteItems.map(item => (
-                    <FavoriteItemItem key={item.id} item={item}/>
-            ))}
-        </Stack>
-        </Container>
+        <>
+            <ToggleButton toggleButtonClicked={toggleButtonClicked} highlighted={'category'} />
+            <Container>
+            <Typography variant="h6">Faves</Typography>
+            <Stack spacing={2}>
+                {favoriteItems.map(item => (
+                        <FavoriteItemItem key={item.id} item={item}/>
+                ))}
+            </Stack>
+            </Container>
+            <Paper sx={{padding: 1, position: 'fixed', bottom: 0, left: 0, right: 0, zIndex: 1000000000, backgroundColor: "transparent" }} elevation={0}>
+                <BottomBar />
+            </Paper>
+        </>
     )
 }
 
--- src/components/ToggleButton/ToggleButton.jsx
+++ src/components/ToggleButton/ToggleButton.jsx
@@ -2,21 +2,20 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import './ToggleButton.css'
-import { useState } from 'react'
 
 
-export default function ToggleButton() {
-  const [isFront, setIsFront] = useState(true)
+export default function ToggleButton({toggleButtonClicked, highlighted}) {
 
     const handleClick = (e) => {
-      console.log('this', e.currentTarget)
-        setIsFront(current => !current)  
+      console.log('this', e.target.value)
+        
+      toggleButtonClicked(e);
     }
 
   return (
     <Stack direction='row' justifyContent='center'>
-      <Button variant='contained' color='baseTan' id='outfitButton' className={isFront ? 'frontButton' : ''} sx={{borderRadius: 3 , width: 120, left: 10, fontSize: 16}} onClick={handleClick}>Outfit</Button>
-      <Button variant='contained' color='basePink' id='categoryButton' className={!isFront ? 'frontButton' : ''} sx={{borderRadius: 3, width: 120, right: 10, fontSize: 16}} onClick={handleClick}>Category</Button>
+      <Button variant='contained' value='outfit' color={highlighted === 'outfit' ? 'basePink' : 'baseTan'} id='outfitButton' className={highlighted === 'outfit' ? 'frontButton' : ''} sx={{borderRadius: 3 , width: 120, left: 10, fontSize: 16}} onClick={handleClick}>Outfit</Button>
+      <Button variant='contained' value='category' color={highlighted === 'category' ? 'basePink' : 'baseTan'} id='categoryButton' className={highlighted === 'category'  ? 'frontButton' : ''} sx={{borderRadius: 3, width: 120, right: 10, fontSize: 16}} onClick={handleClick}>Category</Button>
     </Stack>
   );
 }
