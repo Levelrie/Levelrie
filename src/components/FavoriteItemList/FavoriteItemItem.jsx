38965332dc8ffc7e8cd39078af3e198608de7fcb
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-11-14 22:15:26 -0600

    item item layout improvements with grid

--- src/components/FavoriteItemList/FavoriteItemItem.jsx
+++ src/components/FavoriteItemList/FavoriteItemItem.jsx
@@ -13,31 +13,77 @@ function FavoriteItemItem({item}) {
     console.log('this is the item:', item);
 
     return (
-        <div key={item.id} variant="outlined" className="itemGrid">
-            <Grid container>
-
-                {/* COLUMN 1 */}
-                <Grid item container xs={4}>
-                    <Stack className='this'>             
-                        <img src={item.img} />
-                        <Typography>{item.price}</Typography>
-                    </Stack> 
-                </Grid>
-
-                {/* COLUMN 2 */}
-                <Grid item container xs={6}>
-                    <Stack>
-                        <Typography variant="body1" gutterBottom>{item.name}</Typography>
-                        <DropDown />
-                    </Stack>
-                </Grid>
-
-                {/* COLUMN 3 */}
-                <Grid xs={2}>       
-                    <BuyCheckbox />
-                </Grid>
-            </Grid>
+        <div className='itemItemContainer'>
+            <div className='itemItemImage'>
+                <img src={item.img} className='itemItemImagePic' />
+            </div>
+            <div className='itemItemPrice'>
+                <Typography className='priceText'>{item.price}</Typography>
+            </div>
+            <div className='itemItemName'>
+                <Typography>{item.name}</Typography>
+            </div>
+            <div className='itemItemColorLabel'>
+                <Typography variant="captiontext" className='ddLabel'>Color:</Typography>
+            </div>
+            <div className='itemItemColorDropDown'>
+                <select name="color" className='selectDropDown'>
+                    <option value="red">red</option>
+                    <option value="blue">blue</option>
+                    <option value="yellow">yellow</option>
+                </select>
+            </div>
+            <div className='itemItemSizeLabel'>
+                <Typography variant="captiontext" className='ddLabel'>Size:</Typography>
+            </div>
+            <div className='itemItemSizeDropDown'>
+                <select name="size" className='selectDropDown'>
+                        <option value="s">s</option>
+                        <option value="m">m</option>
+                        <option value="l">l</option>
+                    </select>
+            </div>
+            <div className='itemItemQuantityLabel'>
+                <Typography variant="captiontext" className='ddLabel'>Qty:</Typography>
+            </div>
+            <div className='itemItemQuantityDropDown'>
+                <select name="quantity" className='selectDropDown'>
+                    <option value="0">0</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                </select>
+            </div>
+            <div className='itemItemBuy'>
+                <BuyCheckbox />
+            </div>
         </div>
+
+
+        // <div key={item.id} variant="outlined" className="itemGrid">
+        //     <Grid container>
+
+               
+        //         <Grid item container xs={4}>
+        //             <Stack className='this'>             
+        //                 <img src={item.img} />
+        //                 <Typography>{item.price}</Typography>
+        //             </Stack> 
+        //         </Grid>
+
+    
+        //         <Grid item container xs={6}>
+        //             <Stack>
+        //                 <Typography variant="body1" gutterBottom>{item.name}</Typography>
+        //                 <DropDown />
+        //             </Stack>
+        //         </Grid>
+
+              
+        //         <Grid xs={2}>       
+        //             <BuyCheckbox />
+        //         </Grid>
+        //     </Grid>
+        // </div>
     );
 };
 
