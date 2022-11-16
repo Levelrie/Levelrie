15fbc137e297e0d97bd1edbcbad38b998be854d1
Chameng Vang <Chameng.vang@outlook.com>
2022-11-15 21:25:33 -0600

    Fixed/update closet details SQL. (hopefully this is the last time).

--- server/routes/closetOutfit.router.js
+++ server/routes/closetOutfit.router.js
@@ -37,18 +37,17 @@ router.get('/', rejectUnauthenticated, (req, res) => {
     // GET route to fetch the selected outfit details information. 
     router.get('/:id', (req, res) => {
       const sqlQuery = `
-        SELECT 
-          closet_outfits.id, 
+        SELECT closet_outfits.*,
           outfits.name,
           outfits.description,
-          JSON_AGG((items, categories.name)) AS items
-        FROM closet_outfits
+          JSON_AGG((items, categories.name)) AS items 
+        FROM "closet_outfits"
         JOIN "outfits" ON closet_outfits.outfit_id = outfits.id
         JOIN "outfit_items" ON outfits.id = outfit_items.outfit_id
-        JOIN "items" ON items.id = outfit_items.id
-        JOIN "categories" ON categories.id = items.category_id
+        JOIN "items" ON outfit_items.item_id = items.id
+        INNER JOIN "categories" ON items.category_id = categories.id
         WHERE closet_outfits.outfit_id = $1
-        GROUP BY closet_outfits.id, closet_outfits.user_id, outfits.id;
+        GROUP BY closet_outfits.id, outfits.id;
     `
     const sqlValues = [req.params.id]
     pool.query(sqlQuery, sqlValues)
