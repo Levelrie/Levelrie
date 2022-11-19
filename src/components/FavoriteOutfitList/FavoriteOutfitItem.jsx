44b324abaec527a1ebf28f9ab5cf34c0987222e3
Theo Janke-Furman <tjankefurman@gmail.com>
2022-11-19 15:20:12 -0600

    Specific item details display

--- src/components/FavoriteOutfitList/FavoriteOutfitItem.jsx
+++ src/components/FavoriteOutfitList/FavoriteOutfitItem.jsx
@@ -5,16 +5,16 @@ function FavoriteOutfitItem({outfit, occasionId}) {
 
     const history = useHistory();
 
-    const handleClick = () => {
+    const handleClick = (itemId) => {
         console.log('in handleClick');
-        history.push(`/favorites/outfits/${occasionId}/${outfit.outfit_id}`);
+        history.push(`/favorites/outfits/${occasionId}/${outfit.outfit_id}/${itemId}`);
     }
 
     return (   
          <div className='faveOutfitContainer'>
             {outfit.items.map(item => {
                 return(
-                    <div key={item.f1.id} id={item.f2} onClick={handleClick}>
+                    <div key={item.f1.id} id={item.f2} onClick={() => handleClick(item.f1.id)}>
                         <img className="itemPic" src={item.f1.img} />
                     </div>
                 )
