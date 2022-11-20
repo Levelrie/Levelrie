f95445f27782308d70864fe5df4533f70c1bd538
Kyle Jensen <kjensen19@gmail.com>
2022-11-20 12:05:18 -0600

    fixed color and size dropdowns, quantity still needs work

--- src/components/CheckoutSelectors/CheckoutLanding.jsx
+++ src/components/CheckoutSelectors/CheckoutLanding.jsx
@@ -50,7 +50,7 @@ export default function CheckoutLanding() {
             <Stack width={1} className='checkoutFrame' justifyItems='center' alignItems='center' mt={3} overflow='scroll'>
                 {cart ? cart.map((item) => {
                 return(
-                    <FavoriteItemItem item={item} key={item.id} sizes={[item.size]} colors={[item.color]}/>
+                    <FavoriteItemItem item={item} key={item.id} category={[item.category]} sizes={[{name: item.name, size: item.size}]} colors={[{name: item.name, color: item.color}]} inItemDetails={true}/>
                 )}): 'None'}
             </Stack>
             <Divider width='80%' color='#F2DCF2' height={32} sx={{borderBottomWidth: 8, mt: 5, mb: 3}} />
